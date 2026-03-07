12464027758115800bf42ae401c5163f355bd69b
Alex Bobrenko <oknerbob@gmail.com>
2026-03-07 22:30:39 +0100

    service/cache (#5)
    * Separate Cache service into a folder rather then init in place in app.modules.ts
    
    * address comment
    
    ---------
    
    Co-authored-by: Alex <alex@lightbeam.ai>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { CacheModule } from "@nestjs/cache-manager";
-import { KeyvCacheableMemory } from "cacheable";
-import { Keyv } from "keyv";
-import KeyvRedis from "@keyv/redis";
 import { DatabaseModule } from "./db/db.module";
+import { CacheModule } from "./cache/cache.module";
 
 @Module({
-  imports: [
-    DatabaseModule,
-    CacheModule.registerAsync({
-      useFactory: () => {
-        return {
-          stores: [
-            new Keyv({
-              store: new KeyvCacheableMemory({ ttl: 60000, lruSize: 5000 }),
-            }),
-            new KeyvRedis(process.env.REDIS_DB_URL ?? "redis://redis:6379"),
-          ],
-        };
-      },
-    }),
-  ],
+  imports: [DatabaseModule, CacheModule],
   controllers: [AppController],
   providers: [AppService],
 })
