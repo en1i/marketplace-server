a371c8134699165d4c832f1dc628b9aa9f51cfde
Alex <alex@lightbeam.ai>
2026-03-08 01:20:05 +0100

    remove test dummy data

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { CACHE_MANAGER, Cache } from "@nestjs/cache-manager";
-import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger("Bootstrap");
-  const cacheManager = app.get<Cache>(CACHE_MANAGER);
+  app.enableShutdownHooks();
 
-  const value = new Date().toISOString();
-  await cacheManager.set("nest_test", value);
-  const testValue: string | undefined = await cacheManager.get("nest_test");
-  logger.log(`Redis set: ${value}`);
-  logger.log(`Redis get: ${testValue}`);
-
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, "0.0.0.0");
 }
 bootstrap().catch((err) => {
   console.error("Error starting the application:", err);
